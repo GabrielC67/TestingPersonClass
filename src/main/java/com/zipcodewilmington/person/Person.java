abcc94930009a26eeb01aa032121948e71895524
GabrielC67 <gabecruzpro@gmail.com>
2025-03-09 16:05:56 -0400

    finished

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,32 +4,48 @@ package com.zipcodewilmington.person;
  * If you did ObjectsOne lab, you might recognize this code?
  */
 public class Person {
-    private String name;
-    private int age;
+    // variables initialized
+    private String name = "";
+    private int age = Integer.MAX_VALUE;
 
-    public Person() {
-    }
-
-    public Person(int age) {
-    }
-
-    public Person(String name) {
-    }
-
+    //Constructor Person name and age
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
+    //Constructor Person with just age
+    public Person(int age) {
+        this.age = age;
+    }
+
+    //Constructor Person with just name
+    public Person(String name) {
+        this.name = name;
+    }
+
+    //default constructor
+    public Person() {
+
+    }
+
+    //Setter for name
     public void setName(String name) {
+        this.name = name;
     }
 
+    //Setter for age
     public void setAge(int age) {
+        this.age = age;
     }
 
+    //Getter for name
     public String getName() {
-        return null;
+        return name;
     }
 
+    //Getter for age
     public Integer getAge() {
-        return null;
+        return age;
     }
 }
